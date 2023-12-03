eea6d864d8e4367cfdb4135631971ba34e8d7b0f
khoshow <khoshow.official@gmail.com>
2023-12-03 19:38:34 +0530

    add gtm

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Aos from "aos";
 import { useEffect } from "react";
 import "aos/dist/aos.css";
 import "../styles/index.scss";
+import Script from "next/script";
 import ScrollToTop from "../components/common/ScrollTop";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
@@ -18,6 +19,18 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        id="google-tag-manager"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PFKLHBCC')`,
+        }}
+      ></Script>
       <Component {...pageProps} />
       {/* <ScrollToTop /> */}
     </>
--- pages/_document.js
+++ pages/_document.js
@@ -8,14 +8,17 @@ export default function Document() {
           name="description"
           content="Test My English Level - Created to upgrade your English skills enjoyable, complimentary, and interactive."
         />
-        <meta
-          name="keywords"
-          content="English Learning"
-        />
+        <meta name="keywords" content="English Learning" />
 
         <link rel="icon" href="/favicon.png" />
       </Head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PFKLHBCC"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        ></noscript>
         <Main />
         <NextScript />
       </body>
